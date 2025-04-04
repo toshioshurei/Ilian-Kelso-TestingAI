aa43459a8d9858bab6439ab8571ec4957efe773b
ilian kelso <toshio.shurei@gmail.com>
2025-04-03 22:48:26 -0600

    Cambio de Proyecto

--- Software/Codigo de la aplicacion/reactjs-tmdb-app/app/scripts/app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app-main';
-import 'whatwg-fetch';
-require('es6-promise').polyfill();
-
-window.React = React;
-
-ReactDOM.render( < App / > , document.getElementById('app'));
